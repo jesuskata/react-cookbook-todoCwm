d9f99fff777d8d6fe67355a2db9233be341eed11
jesuskata <rzarate.alex@gmail.com>
2018-10-16 00:58:05 -0500

    Este commit es para subir el proyecto casi completo. Me encontré con error que no logro solucionar, relacionado a la destructuración.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todo from './Todo/Todo';
 import Header from '../shared/components/layout/Header';
 import Content from '../shared/components/layout/Content';
 import Home from './Home/Home';
@@ -9,10 +10,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header title="Welcome to React"/>
+        <Header title="Todo List"/>
+
         <Content>
-          <Home />
+          <Todo />
         </Content>
+
         <Footer />
       </div>
     );
